47c6149d2c3e9ba08fead951505257a5e0b2166d
guen <guen5997@naver.com>
2023-11-03 17:51:36 +0900

    Create: Application class

---
+++ app/src/main/java/com/example/namecompatibility/di/App.kt
@@ -0,0 +1,16 @@
+package com.example.namecompatibility.di
+
+import android.app.Application
+
+
+class App : Application() {
+    companion object {
+        private lateinit var applicaton: App
+        fun getInstance(): App = applicaton
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        applicaton = this
+    }
+}
