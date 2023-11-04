ea204e1e884a75a7fd88f80f5556ffbbb5608463
guen <guen5997@naver.com>
2023-11-04 14:39:16 +0900

    Create: Retrofit Http Client

--- presentation/src/main/java/com/example/presentation/view/MainActivity.kt
+++ presentation/src/main/java/com/example/presentation/view/MainActivity.kt
@@ -4,12 +4,11 @@ import androidx.activity.viewModels
 import com.example.presentation.R
 import com.example.presentation.base.BaseActivity
 import com.example.presentation.databinding.ActivityMainBinding
-import com.example.presentation.view.viewMdel.MainViewModel
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
+
+//@AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
-    private val mainViewModel by viewModels<MainViewModel>()
+   // private val mainViewModel by viewModels<MainViewModel>()
     override fun init() {
 
 
